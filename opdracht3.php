c9097910cfc84fac4e725448a12a1974e6d39096
Efe Korumaz <9022358@student.tcrmbo.nl>
2024-09-26 16:16:22 +0000

    Update opdracht3.php

--- opdracht3.php
+++ opdracht3.php
@@ -1,31 +1,29 @@
 <?php
-
 class Product
 {
-public $name;
-public $price;
+    public $name;
+    public $price;
 
-    public function formatPrice()
+    public function showSalesPrice()
     {
-        return number_format( $this->price, 2);
+        $salesPrice = $this->price * 1.09; 
+        return number_format($salesPrice, 2); 
     }
-
 }
 
-$game1 = new product;
+$game1 = new Product;
 $game1->name = "fifa";
-$game1->price = 70;
+$game1->price = 60;
 
-$game2 = new product();
-$game2 ->name = "hollow knight";
+$game2 = new Product();
+$game2->name = "hollow knight";
 $game2->price = 15;
 
-
-$game3 = new product();
+$game3 = new Product();
 $game3->name = "pokemon";
 $game3->price = 60;
 
-
-echo $game1-> name . $game1->formatPrice(). '<br>' ;
-echo $game2->name . '<br>';
-echo $game3->name . '<br>';
+echo $game1->name . " - €" . $game1->showSalesPrice() . '<br>';
+echo $game2->name . " - €" . $game2->showSalesPrice() . '<br>';
+echo $game3->name . " - €" . $game3->showSalesPrice() . '<br>';
+?>
