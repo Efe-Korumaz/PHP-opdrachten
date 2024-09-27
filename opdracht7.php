87a883310192919b4ca724f7f164d8331ac9b222
Efe Korumaz <9022358@student.tcrmbo.nl>
2024-09-27 07:16:38 +0000

    Upload New File

---
+++ opdracht7.php
@@ -0,0 +1,38 @@
+<?php
+class Product
+{
+
+
+    public function __construct(public $name, public $description, public $category = "dranken", public $rating = 3)
+    {
+        $this->name = strtolower($name);
+        $this->description = strtolower($description);
+        $this->category = strtoupper($category);
+    }
+
+    public function showSalesPrice()
+    {
+        $salesPrice = $this->price * 1.09;
+        return number_format($salesPrice, 2);
+    }
+
+    public function showDescription()
+    {
+        return ucfirst($this->description) . " - Rating: " . $this->rating . "/5";
+    }
+}
+
+$game1 = new Product(name: "fifa", description: "it's a game about football where you play as a team");
+$game1->price = 60;
+
+$game2 = new Product(name: "hollow knight", description: "explore the world of hallownest and unravel the mysteries plaguing these lands", category: "exploration");
+$game2->price = 15;
+
+$game3 = new Product(name: "pokemon", description: "a magical world full of mysterious creatures", rating: 5);
+$game3->price = 60;
+
+echo $game1->name . " - €" . $game1->showSalesPrice() . " " . $game1->category . " - " . $game1->showDescription() . '<br>';
+echo $game2->name . " - €" . $game2->showSalesPrice() . " " . $game2->category . " - " . $game2->showDescription() . '<br>';
+echo $game3->name . " - €" . $game3->showSalesPrice() . " " . $game3->category . " - " . $game3->showDescription() . '<br>';
+?>
+0
